da7f5be918144203ef932ea7a6d80777bf996c54
eluna2007 <eluna2007@gmail.com>
2017-12-20 12:47:10 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,9 +8,9 @@
 # Recipe:: enable_sudo_no_tty
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-#if node['os'] == 'linux'
-#    include_recipe 'selinux::permissive'
-#end
+if node['os'] == 'linux'
+    include_recipe 'selinux::permissive'
+end
 execute 'Set !require tty for kitchen user' do
     action :run
     command 'echo "Defaults!ALL !requiretty" >> /etc/sudoers.d/kitchen'
